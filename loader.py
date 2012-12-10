606641151624f502fb8a9e59019eb73158db247e
sasaki-k <siroken3@gmail.com>
2012-12-10 12:29:18 +0900

    jinja templateから直接記述するようにした。

--- loader.py
+++ loader.py
@@ -51,17 +51,6 @@ class CloudFormer:
         route_tables = parse_context['route_tables']
         associations = []
         for route_table in route_tables:
-            associations.extend([CfnSubnetRouteTableAssociation(assoc) for assoc in route_table.associations])
-        parse_context['subnet_route_table_association'] = associations
+            associations.extend([CfnSubnetRouteTableAssociation(assoc) for assoc in route_table.value.associations])
+        parse_context['subnet_route_table_associations'] = associations
 
-if __name__ == '__main__':
-    import os
-    parser = argparse.ArgumentParser()
-    parser.add_argument('vpcid')
-    parser.add_argument('-O','--aws-access-key')
-    parser.add_argument('-W','--aws-secret-key')
-    parsed = parser.parse_args()
-    access_key = parsed.aws_access_key if parsed.aws_access_key is not None else os.environ.get('AWS_ACCESS_KEY')
-    secret_key = parsed.aws_secret_key if parsed.aws_secret_key is not None else os.environ.get('AWS_SECRET_KEY')
-    former = CloudFormer(vpc_id=parsed.vpcid, access_key=access_key, secret_key=secret_key)
-    print [(k, str(v)) for k, v in former.form().items()]
--- models.py
+++ models.py
@@ -9,54 +9,29 @@ class CfnVpc(CfnAWSResource):
     typestring = 'AWS::EC2::VPC'
 
     def __init__(self, vpc):
-        self.vpc = vpc
-        self.cidr_block = vpc.cidr_block
-        self.instanceTenancy = vpc.instanceTenancy
-
-    def __str__(self):
-        return """
-                   "MyVPC": {
-                      "Type" : "%(type)s",
-                      "Properties": {
-                          "CidrBlock": %(cidr)s,
-                          "InstanceTenancy": %(tenancy)s
-                       }
-                   }
-               """ % {'type': self.typestring,'cidr': self.cidr_block,'tenancy':self.instanceTenancy}
+        self.value = vpc
 
 class CfnSubnet(CfnAWSResource):
     typestring = 'AWS::EC2::Subnet'
 
     def __init__(self, subnet):
-        self.subnet = subnet
-
-    def __str__(self):
-        return "hoge"
-        return """
-                   "MySubnet": {
-                       "Type" : "%(type)s",
-                       "Properties" : {
-                           "AvailabilityZone" : %(zone)s,
-                           "CidrBlock" : %(cidr)s,
-                           "VpcId" : { "Ref" : %(vpc)s }
-                       }
-                   }
-               """
+        self.value = subnet
 
 class CfnRouteTable(CfnAWSResource):
     typestring = 'AWS::EC2::RouteTable'
 
     def __init__(self, route_table):
-        self.route_table = route_table
-        self.associations = route_table.associations
-
+        self.value = route_table
 
 class CfnSubnetRouteTableAssociation(CfnAWSResource):
     typestring = 'AWS::EC2::SubnetRouteTableAssociation'
 
     def __init__(self, route_table_association):
-        self.route_table_association = route_table_association
-        self.route_table_id = route_table_association.route_table_id
-        self.subnet_id = route_table_association.subnet_id
+        self.value = route_table_association
 
+class CfnRoute(CfnAWSResource):
+    typestring = 'AWS::EC2::Route'
+
+    def __init__(self, route):
+        self.value = route
 
