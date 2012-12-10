5b8dceb6420cbe211ab74c5019473e67fdddb4c0
sasaki-k <siroken3@gmail.com>
2012-12-11 08:49:51 +0900

    Refが入ってなかったので修正

--- lib/floccus/main.py
+++ lib/floccus/main.py
@@ -13,7 +13,7 @@ parsed = parser.parse_args()
 access_key = parsed.aws_access_key if parsed.aws_access_key is not None else os.environ.get('AWS_ACCESS_KEY')
 secret_key = parsed.aws_secret_key if parsed.aws_secret_key is not None else os.environ.get('AWS_SECRET_KEY')
 former = CloudFormer(vpc_id=parsed.vpcid, access_key=access_key, secret_key=secret_key)
-env = Environment(loader=PackageLoader(__name__, 'templates'))
+env = Environment(loader=PackageLoader(__name__, 'templates'), trim_blocks='True')
 metatemplate = env.get_template('metatemplate.jinja2')
 model = former.form()
 print metatemplate.render(model=model);
