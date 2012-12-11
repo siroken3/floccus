bdd53b961a8331c11ce275a0ba768f1d24bf7863
sasaki-k <siroken3@gmail.com>
2012-12-12 03:13:49 +0900

    templateの中で .value. を付けなくてもよくした。その他リファクタリング

--- lib/floccus/former.py
+++ lib/floccus/former.py
@@ -16,6 +16,7 @@ class CloudFormer:
             aws_access_key_id=self.access_key,
             aws_secret_access_key=self.secret_key
             )
+        self.vpc_id = vpc_id
         self.vpc_filter = ('vpc-id', vpc_id)
         self.vpc_attachment_filter = ('attachment.vpc-id', vpc_id)
 
@@ -25,56 +26,64 @@ class CloudFormer:
             aws_access_key_id=self.access_key,
             aws_secret_access_key=self.secret_key
             )
-        parse_context = {}
-        self._form_vpc(parse_context)
-        self._form_internet_gateway(parse_context)
-        self._form_gateway_attachments(parse_context)
-        self._form_subnets(parse_context)
-        self._form_route_tables(parse_context)
-        self._form_route(parse_context)
-        self._form_subnet_route_table_association(parse_context)
-        return parse_context
+        context = {}
+        self._form_vpc(context)
+        self._form_internet_gateway(context)
+        self._form_gateway_attachments(context)
+        self._form_subnets(context)
+        self._form_route_tables(context)
+        self._form_instances(context)
+        self._form_route(context)
+        self._form_subnet_route_table_association(context)
+        return context
 
-    def _form_vpc(self, parse_context):
+    def _form_vpc(self, context):
         vpcs = self.vpcconn.get_all_vpcs(filters=[self.vpc_filter])
-        parse_context['vpc'] = CfnVpc(vpcs[0])
+        context['vpc'] = CfnVpc(vpcs[0])
 
-    def _form_internet_gateway(self, parse_context):
-        parse_context['internet_gateways'] = [CfnInternetGateWay(igw) for igw
+    def _form_internet_gateway(self, context):
+        context['internet_gateways'] = [CfnInternetGateWay(igw) for igw
                                               in self.vpcconn.get_all_internet_gateways(
                 filters=[self.vpc_attachment_filter]
                 )]
 
-    def _form_gateway_attachments(self, parse_context):
-        internet_gateways = parse_context['internet_gateways']
+    def _form_gateway_attachments(self, context):
+        internet_gateways = context['internet_gateways']
         attachments = []
         for internet_gateway in internet_gateways:
-            attachments.extend([CfnVpcGatewayAttachment(att, internet_gateway) for att in internet_gateway.value.attachments])
-        parse_context['gateway_attachments'] = attachments
+            attachments.extend([CfnVpcGatewayAttachment(att, internet_gateway) for att in internet_gateway.attachments])
+        context['gateway_attachments'] = attachments
 
-    def _form_subnets(self, parse_context):
-        parse_context['subnets'] = [CfnSubnet(s) for s
+    def _form_subnets(self, context):
+        context['subnets'] = [CfnSubnet(s) for s
                                     in self.vpcconn.get_all_subnets(
                 filters=[self.vpc_filter]
                 )]
 
-    def _form_route_tables(self, parse_context):
-        parse_context['route_tables'] = [CfnRouteTable(rtb) for rtb
+    def _form_route_tables(self, context):
+        context['route_tables'] = [CfnRouteTable(rtb) for rtb
                                          in self.vpcconn.get_all_route_tables(
                 filters=[self.vpc_filter]
                 )]
 
-    def _form_route(self, parse_context):
-        route_tables = parse_context['route_tables']
+    def _form_instances(self, context):
+        instances = []
+        for reservation in self.vpcconn.get_all_instances(filters={'vpc-id': self.vpc_id}):
+            for instance in reservation.instances:
+                instances.append(CfnEC2Instance(instance))
+        context['instances'] = instances
+
+    def _form_route(self, context):
+        route_tables = context['route_tables']
         routes = []
         for route_table in route_tables:
-            routes.extend([CfnRoute(route, route_table) for route in route_table.value.routes])
-        parse_context['routes'] = routes
+            routes.extend([CfnRoute(route, route_table) for route in route_table.routes])
+        context['routes'] = routes
 
-    def _form_subnet_route_table_association(self, parse_context):
-        route_tables = parse_context['route_tables']
+    def _form_subnet_route_table_association(self, context):
+        route_tables = context['route_tables']
         associations = []
         for route_table in route_tables:
-            associations.extend([CfnSubnetRouteTableAssociation(assoc) for assoc in route_table.value.associations])
-        parse_context['subnet_route_table_associations'] = associations
+            associations.extend([CfnSubnetRouteTableAssociation(assoc) for assoc in route_table.associations])
+        context['subnet_route_table_associations'] = associations
 
--- lib/floccus/models.py
+++ lib/floccus/models.py
@@ -1,50 +1,40 @@
 class CfnAWSResource:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, value):
+        self.__value = value
 
-    def __str__(self):
-        return self.typestring
+    def __getattr__(self, name):
+        return getattr(self.__value, name)
 
 class CfnVpc(CfnAWSResource):
-    typestring = 'AWS::EC2::VPC'
-
     def __init__(self, vpc):
-        self.value = vpc
+        CfnAWSResource.__init__(self, vpc)
 
 class CfnInternetGateWay(CfnAWSResource):
-    typestring = 'AWS::EC2::InternetGateway'
-
     def __init__(self, internet_gateway):
-        self.value = internet_gateway
+        CfnAWSResource.__init__(self, internet_gateway)
 
 class CfnVpcGatewayAttachment(CfnAWSResource):
-    typestring = 'AWS::EC2::VPCGatewayAttachment'
-
     def __init__(self, attachment, gateway):
-        self.value = attachment
+        CfnAWSResource.__init__(self, attachment)
         self.gateway = gateway
 
 class CfnSubnet(CfnAWSResource):
-    typestring = 'AWS::EC2::Subnet'
-
     def __init__(self, subnet):
-        self.value = subnet
+        CfnAWSResource.__init__(self, subnet)
 
 class CfnRouteTable(CfnAWSResource):
-    typestring = 'AWS::EC2::RouteTable'
-
     def __init__(self, route_table):
-        self.value = route_table
+        CfnAWSResource.__init__(self, route_table)
 
 class CfnSubnetRouteTableAssociation(CfnAWSResource):
-    typestring = 'AWS::EC2::SubnetRouteTableAssociation'
-
     def __init__(self, route_table_association):
-        self.value = route_table_association
+        CfnAWSResource.__init__(self, route_table_association)
+
+class CfnEC2Instance(CfnAWSResource):
+    def __init__(self, instance):
+        CfnAWSResource.__init__(self, instance)
 
 class CfnRoute(CfnAWSResource):
-    typestring = 'AWS::EC2::Route'
-
     def __init__(self, route, route_table):
-        self.value = route
+        CfnAWSResource.__init__(self, route)
         self.route_table = route_table
