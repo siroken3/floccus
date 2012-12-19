d6ce3cd6369069f2c960815349393c1d59860f34
sasaki-k <siroken3@gmail.com>
2012-12-19 21:08:27 +0900

    issue #23

--- lib/floccus/main.py
+++ lib/floccus/main.py
@@ -12,8 +12,8 @@ def main():
 # parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('vpcid')
-    parser.add_argument('-O','--aws-access-key')
-    parser.add_argument('-W','--aws-secret-key')
+    parser.add_argument('-O','--aws-access-key', default=None)
+    parser.add_argument('-W','--aws-secret-key', default=None)
     parser.add_argument('--region')
     parsed = parser.parse_args()
     access_key, secret_key = utils.get_aws_key(parsed.aws_access_key, parsed.aws_secret_key)
